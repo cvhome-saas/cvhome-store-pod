4720f24f55559ac149f4e8db8c7bf987758e8456
Ashraf Mahmoud <ashraf1abdlrasool@gmail.com>
2025-11-30 01:12:00 +0300

    cloudfront

--- store-pod-storage.tf
+++ store-pod-storage.tf
@@ -23,6 +23,7 @@ module "cert-storage-bucket" {
 
 module "cdn-storage-cloudfront" {
   source = "terraform-aws-modules/cloudfront/aws"
+  version = "5.2.0"
 
 
   comment             = "cdn storage cloudfront distribution"
@@ -43,17 +44,11 @@ module "cdn-storage-cloudfront" {
   # This rate is charged only once per month, per metric (up to 8 metrics per distribution).
   create_monitoring_subscription = true
 
-
-  origin_access_control = {
-    s3 = {
-      description      = "CloudFront access to S3"
-      origin_type      = "s3"
-      signing_behavior = "always"
-      signing_protocol = "sigv4"
-    }
+  create_origin_access_identity = true
+  origin_access_identities = {
+    cdn_storage_access_identity = "My awesome CloudFront can access"
   }
-  
-  
+
   # create_origin_access_control = false
   # origin_access_control = {
   #   cdn_storage = {
@@ -68,11 +63,10 @@ module "cdn-storage-cloudfront" {
     cdn_storage = {
       # with origin access identity (legacy)
       domain_name = module.cdn-storage-bucket.s3_bucket_bucket_regional_domain_name
-      origin_access_control_key = "s3" # key in `origin_access_control`
-      # s3_origin_config = {
-      #   origin_access_identity = "cdn_storage_access_identity" # key in `origin_access_identities`
-      #   # cloudfront_access_identity_path = "origin-access-identity/cloudfront/E5IGQAA1QO48Z" # external OAI resource
-      # }
+      s3_origin_config = {
+        origin_access_identity = "cdn_storage_access_identity" # key in `origin_access_identities`
+        # cloudfront_access_identity_path = "origin-access-identity/cloudfront/E5IGQAA1QO48Z" # external OAI resource
+      }
     }
   }
 
@@ -110,6 +104,17 @@ module "cdn-storage-cloudfront" {
 }
 
 data "aws_iam_policy_document" "cdn_storage_bucket_policy" {
+  # Origin Access Identities
+  statement {
+    actions   = ["s3:GetObject"]
+    resources = ["${module.cdn-storage-bucket.s3_bucket_arn}/*"]
+
+    principals {
+      type        = "AWS"
+      identifiers = module.cdn-storage-cloudfront.cloudfront_origin_access_identity_iam_arns
+    }
+  }
+
   # Origin Access Controls
   statement {
     actions   = ["s3:GetObject"]
