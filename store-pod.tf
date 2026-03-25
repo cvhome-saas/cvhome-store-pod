ea47e2792f8cd1eea9bea9d65c448f9d83ad7d2c
ashraf <ashraf1abdelrasool@gmail.com>
2026-03-26 01:18:15 +0200

    Fix environment variable name for POD_ID in store-pod-cluster configuration.

--- store-pod.tf
+++ store-pod.tf
@@ -1,6 +1,4 @@
 locals {
-  shorten_pod_id_hash = substr(md5(var.pod.id), 0, 3)
-  shorten_pod         = "${var.pod.shorten_pod_id}-${local.shorten_pod_id_hash}"
-  simple_module_name  = "pod-${local.shorten_pod}"
+  simple_module_name  = "store-pod-${var.pod.shorten_pod_id}"
   pod_record          = "${var.pod.pod_record_prefix}.${var.domain_zone_name}"
 }
